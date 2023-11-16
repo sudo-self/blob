a6a8a847869445552bb5f244641bd853ef18388a
Sudo-Self <sudo-self@proton.me>
2023-11-15 21:18:00 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
     <main className="relative flex min-h-screen flex-col items-center justify-center">
       <Toaster />
       <Link
-        href="https://vercel.com/templates/next.js/blob-starter"
+        href="https://iframe.JesseJesse.com"
         className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
       >
-        <p>Deploy your own to Vercel</p>
+        <p>Generate iFrame</p>
         <ExpandingArrow />
       </Link>
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
